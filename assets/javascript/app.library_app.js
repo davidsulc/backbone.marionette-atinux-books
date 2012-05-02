3331130f30599156137f34df72da303501228ba7
davidsulc <david@davidsulc.com>
2012-05-02 12:11:57 -0400

    fetch books

--- assets/javascript/app.library_app.js
+++ assets/javascript/app.library_app.js
@@ -10,6 +10,88 @@ MyApp.LibraryApp = function(){
     }
   });
   
+  var Book = Backbone.Model.extend();
+
+  var Books = Backbone.Collection.extend({
+    model: Book,
+    
+    initialize: function(){
+      var self = this;
+      
+      // the number of books we fetch each time
+      this.maxResults = 40;
+      // the results "page" we last fetched
+      this.page = 0;
+      
+      // flags whether the collection is currently in the process of fetching
+      // more results from the API (to avoid multiple simultaneous calls
+      this.loading = false;
+      
+      // the maximum number of results for the previous search
+      this.totalItems = null;
+    },
+    
+    search: function(searchTerm){
+      this.page = 0;
+      
+      var self = this;
+      this.fetchBooks(searchTerm, function(books){
+        console.log(books);
+      });
+    },
+    
+    fetchBooks: function(searchTerm, callback){
+      if(this.loading) return true;
+
+      this.loading = true;
+      
+      var self = this;
+      MyApp.vent.trigger("search:start");
+      
+      var query = encodeURIComponent(searchTerm)+'&maxResults='+this.maxResults+'&startIndex='+(this.page * this.maxResults)+'&fields=totalItems,items(id,volumeInfo/title,volumeInfo/subtitle,volumeInfo/authors,volumeInfo/publishedDate,volumeInfo/description,volumeInfo/imageLinks)';
+      
+      $.ajax({
+        url: 'https://www.googleapis.com/books/v1/volumes',
+        dataType: 'jsonp',
+        data: 'q='+query,
+        success: function (res) {
+          MyApp.vent.trigger("search:stop");
+          if(res.totalItems == 0){
+            callback([]);
+            return [];
+          }
+          if(res.items){
+            self.page++;
+            self.totalItems = res.totalItems;
+            var searchResults = [];
+            _.each(res.items, function(item){
+              var thumbnail = null;
+              if(item.volumeInfo && item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.thumbnail){
+                thumbnail = item.volumeInfo.imageLinks.thumbnail;
+              }
+              searchResults[searchResults.length] = new Book({
+                thumbnail: thumbnail,
+                title: item.volumeInfo.title,
+                subtitle: item.volumeInfo.subtitle,
+                description: item.volumeInfo.description,
+                googleId: item.id
+              });
+            });
+            callback(searchResults);
+            self.loading = false;
+            return searchResults;
+          }
+          else if (res.error) {
+            MyApp.vent.trigger("search:error");
+            self.loading = false;
+          }
+        }
+      });
+    }
+  });
+  
+  LibraryApp.Books = new Books();
+  
   LibraryApp.initializeLayout = function(){
     LibraryApp.layout = new Layout();
 
@@ -18,6 +100,11 @@ MyApp.LibraryApp = function(){
     });
     MyApp.content.show(MyApp.LibraryApp.layout);
   };
+  
+  // for testing purposes
+  MyApp.addInitializer(function(){
+    LibraryApp.Books.search("Neuromarketing");
+  });
   
   return LibraryApp;
 }();
