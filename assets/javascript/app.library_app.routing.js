7d827960c3488ca05919f89714b49c2cc7bafb99
davidsulc <david@davidsulc.com>
2012-05-03 15:22:25 -0400

    navigate to search term

--- assets/javascript/app.library_app.routing.js
+++ assets/javascript/app.library_app.routing.js
@@ -8,6 +8,10 @@ MyApp.LibraryRouting = function(){
     }
   });
 
+  MyApp.vent.on("search:term", function(searchTerm){
+    Backbone.history.navigate("search/" + searchTerm);
+  });
+
   MyApp.addInitializer(function(){
     LibraryRouting.router = new LibraryRouting.Router({
       controller: MyApp.LibraryApp
